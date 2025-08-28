d768b2e1469e5c19c5534e54a91e5f30d7520aef
Jakub Kostka <jakub.kostka@soneta.pl>
2025-08-28 09:29:58 +0200

    baseline v1: parse test

---
+++ src/Contracts/Numer.cs
@@ -0,0 +1,12 @@
+﻿public class Test
+{
+    public string Kraj { get; set; } = "";
+    public string CS { get; set; } = "";
+    public string Kierunek { get; set; } = "";
+    public string Numer { get; set; } = "";
+
+    public void Parse(string ss)
+    {
+        Numer = ss;
+    }
+}
