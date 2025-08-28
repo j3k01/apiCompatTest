604c4bb062a185585d6319b7ecdd2a73aca81bc0
Jakub Kostka <jakub.kostka@soneta.pl>
2025-08-28 09:58:40 +0200

    test

--- src/MainApp/Program.cs
+++ src/MainApp/Program.cs
@@ -1,7 +1,3 @@
 ﻿using B;
-using Contracts;
 
-ICalculator calc = new Calculator();
-
-Console.WriteLine($"Result: {calc.Add(5)}");
-Console.WriteLine($"Result: {calc.Add(6)}");
+Console.WriteLine("Works");
