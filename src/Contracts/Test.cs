fa76570f0345b89d01dcddae471916d93dcd4467
Jakub Kostka <jakub.kostka@soneta.pl>
2025-08-28 10:45:56 +0200

    test

--- src/Contracts/Test.cs
+++ src/Contracts/Test.cs
@@ -1,17 +1,26 @@
-﻿using Demo.Contracts;
-
-public class Test : ITest
+﻿public class Test
 {
     public string Country { get; set; } = string.Empty;
     public string BankCode { get; set; } = string.Empty;
     public string Branch { get; set; } = string.Empty;
     public string Number { get; set; } = string.Empty;
-}
 
-public static class TestExtensions
-{
-    public static void Parse(this ITest nr, string text)
-    {
-        nr.Number = text;
-    }
+    public void Parse(string ss) { Number = ss; } 
 }
+//using Demo.Contracts;
+
+//public class Test : ITest
+//{
+//    public string Country { get; set; } = string.Empty;
+//    public string BankCode { get; set; } = string.Empty;
+//    public string Branch { get; set; } = string.Empty;
+//    public string Number { get; set; } = string.Empty;
+//}
+
+//public static class TestExtensions
+//{
+//    public static void Parse(this ITest nr, string text)
+//    {
+//        nr.Number = text;
+//    }
+//}
